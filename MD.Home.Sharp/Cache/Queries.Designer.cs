ddf2313725535401855e1b70f15ce69dc13a66d0
Micael Valentim <lord.valentim@gmail.com>
2020-12-05 22:57:15 -0300

    bump cache_size

--- MD.Home.Sharp/Cache/Queries.Designer.cs
+++ MD.Home.Sharp/Cache/Queries.Designer.cs
@@ -115,7 +115,7 @@ namespace MD.Home.Sharp.Cache {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to PRAGMA cache_size=-51200;.
+        ///   Looks up a localized string similar to PRAGMA cache_size=-512000;.
         /// </summary>
         internal static string SetCacheSize {
             get {
