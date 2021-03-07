fd9af619c697c6ee1050ac4897b1a69d26c0f6ff
Micael Valentim <lord.valentim@gmail.com>
2021-03-07 10:03:57 -0300

    Start project rewrite

--- MD.Home.Sharp/Exceptions/ClientSettingsException.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace MD.Home.Sharp.Exceptions
-{
-    internal sealed class ClientSettingsException : Exception
-    {
-        public ClientSettingsException(string message) : base(message) { }
-    }
-}
